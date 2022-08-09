7a2c9e7aba43eeb691b62e5629708876ca59d416
KatKlee <katklee2022@gmail.com>
2022-08-09 13:17:03 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* 3 Möglichkeiten Arrays zu erstellen */
+
+// Möglichkeit 1
+const meineTrainer1 = ['Eric', 'Steffen', 'Simon', 'Ahmed']
+console.log(meineTrainer1)
+
+// Möglichkeit 2
+const meineTrainer2 = new Array('Max', 'Steffen', 'Simon', 'Ahmed')
+console.log(meineTrainer2)
+
+// Möglichkeit 3
+const meineTrainer3 = new Array()
+meineTrainer3[0] = 'Erich'
+meineTrainer3[1] = 'Steffen'
+meineTrainer3[2] = 'Simon'
+meineTrainer3[3] = 'Ahmed'
+console.log(meineTrainer3)
